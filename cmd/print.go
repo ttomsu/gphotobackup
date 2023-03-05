a17e948682f680bb5425277b800d42644a674e50
Travis Tomsu <ttomsu@netflix.com>
2023-03-05 13:03:28 -0500

    rm dumping client

--- cmd/print.go
+++ cmd/print.go
@@ -7,8 +7,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/ttomsu/gphoto-sync/internal"
-	"net/http"
-	"net/http/httputil"
 )
 
 // printCmd represents the print command
@@ -39,27 +37,4 @@ var printCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(printCmd)
-}
-
-func dumpingClient(client *http.Client) *http.Client {
-	dt := &dumpingTransport{
-		client.Transport,
-	}
-	client.Transport = dt
-	return client
-}
-
-type dumpingTransport struct {
-	http.RoundTripper
-}
-
-func (d dumpingTransport) RoundTrip(request *http.Request) (*http.Response, error) {
-	dReq, _ := httputil.DumpRequest(request, false)
-	fmt.Printf("Dumped Request:\n%v\n", string(dReq))
-	resp, err := d.RoundTrip(request)
-	if resp != nil {
-		dRes, _ := httputil.DumpResponse(resp, false)
-		fmt.Printf("Dumped Response:\n%v\n", string(dRes))
-	}
-	return resp, err
 }
